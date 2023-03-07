4608db4b5940059878c78c6b23664cfa35d4ec48
abuzarbt2002 <abuzarbt2002@yahoo.com>
2023-03-07 13:13:06 -0500

    added Photo model to admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 # import your models here
-from .models import Finch, Feeding, Features
+from .models import Finch, Feeding, Features, Photo
 
 # Register your models here
 admin.site.register(Finch)
 admin.site.register(Feeding)
 admin.site.register(Features)
+admin.site.register(Photo)
